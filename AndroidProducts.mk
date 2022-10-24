b8b0a46b5fd718da45372357750b029daff752fa
keremgokcek <mehmetkeremgokcek@outlook.com>
2022-10-24 23:17:11 +0300

    tucana: PixelOS bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_tucana.mk
+    $(LOCAL_DIR)/aosp_tucana.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_tucana-user \
-    arrow_tucana-userdebug \
-    arrow_tucana-eng
+    aosp_tucana-user \
+    aosp_tucana-userdebug \
+    aosp_tucana-eng
--- aosp_tucana.mk
+++ aosp_tucana.mk
@@ -11,17 +11,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from tucana device
 $(call inherit-product, device/xiaomi/tucana/device.mk)
 
-# Inherit some common Arrow stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common PixelOS stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+$(call inherit-product, vendor/aosp/config/common.mk)
 
-# Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_tucana
+# Device identifier
+PRODUCT_NAME := aosp_tucana
 PRODUCT_DEVICE := tucana
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi Note 10
 PRODUCT_MANUFACTURER := Xiaomi
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# ArrowOS Properties
-DEVICE_MAINTAINER := Erikdrozina
